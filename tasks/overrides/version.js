47debb729dcb5d9ffc36580d753d610fc83a75ac
Matt Karl <matt@cloudkid.com>
2014-12-02 09:16:29 -0500

    Bumped version

--- tasks/overrides/version.js
+++ tasks/overrides/version.js
@@ -1,18 +1,16 @@
 module.exports = {
 	options: {
-		files : {
-			'deploy/package.json' : 'version',
-			'installer/win.nsi' : function(contents, version) {
+		'deploy/package.json' : 'version',
+		'installer/win.nsi' : function(contents, version) {
 
-				// Strip off any "-alpha" "-beta" "-rc" etc
-				var extra = version.lastIndexOf('-');
-				var parts = (extra > -1 ? version.substr(0, extra) : version).split('.');
+			// Strip off any "-alpha" "-beta" "-rc" etc
+			var extra = version.lastIndexOf('-');
+			var parts = (extra > -1 ? version.substr(0, extra) : version).split('.');
 
-				// Replace in the file contents and return
-				return contents.replace(/(\!define VERSIONMAJOR) [0-9]+/, "$1 " + parts[0])
-					.replace(/(\!define VERSIONMINOR) [0-9]+/, "$1 " + parts[1])
-					.replace(/(\!define VERSIONBUILD) [0-9]+/, "$1 " + parts[2]);
-			}
+			// Replace in the file contents and return
+			return contents.replace(/(\!define VERSIONMAJOR) [0-9]+/, "$1 " + parts[0])
+				.replace(/(\!define VERSIONMINOR) [0-9]+/, "$1 " + parts[1])
+				.replace(/(\!define VERSIONBUILD) [0-9]+/, "$1 " + parts[2]);
 		}
 	}
 };
